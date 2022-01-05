94e0bd8db3ac8bd36ff521d551abc4853967e30f
Erick Rocha <54082836+zartqrom@users.noreply.github.com>
2022-01-05 15:48:41 -0600

    Corrección de altura de la caja de colisión edificio 1

--- Proyecto/src/main.cpp
+++ Proyecto/src/main.cpp
@@ -1077,8 +1077,8 @@ void applicationLoop() {
 		modelmatrixColliderEdi1 = glm::translate(modelmatrixColliderEdi1,
 				glm::vec3(modelEdi1.getObb().c.x,
 						modelEdi1.getObb().c.y + 77.5,
-						modelEdi1.getObb().c.z + 110.0));
-		edi1Collider.e = modelEdi1.getObb().e * glm::vec3(1.0, 10.8, 0.01);
+						modelEdi1.getObb().c.z + 123.5));
+		edi1Collider.e = modelEdi1.getObb().e * glm::vec3(1.0, 10.8, 0.025);
 		edi1Collider.c = glm::vec3(modelmatrixColliderEdi1[3]);
 		addOrUpdateColliders(collidersOBB, "edi1", edi1Collider, modelMatrixEdi1);
 
